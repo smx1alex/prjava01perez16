563e284c8434193f2fe5fccee97dd7c518ee8227
User <User@192.168.1.180>
2016-05-18 19:52:56 +0200

    Versio '0.2 del projecte prjava01perez16

--- src/prjava01perez16/Prjava01perez16.java
+++ src/prjava01perez16/Prjava01perez16.java
@@ -25,11 +25,11 @@ public class Prjava01perez16 {
             bw.write("<html>");bw.newLine();
             bw.write("  <head>");bw.newLine();
             bw.write("    <title>");bw.newLine();
-            bw.write("      Nova p&agrave;gina web de Alejandro Perez");bw.newLine();
+            bw.write("      Nova p&agrave;gina web de Alejandro Perez DAW2 UF4 practica 2");bw.newLine();
             bw.write("    </title>");bw.newLine();
             bw.write("  </head>");bw.newLine();
             bw.write("  <body>");bw.newLine();
-            bw.write("    Nova p&agrave;gina web de Alejandro Perez");bw.newLine();
+            bw.write("    Nova p&agrave;gina web de Alejandro Perez DAW2 UF4 practica 2");bw.newLine();
             bw.write("  </body>");bw.newLine();
             bw.write("</html>");bw.newLine();
             bw.close();
